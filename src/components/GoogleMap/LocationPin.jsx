fa9d5c5da43565ef134ae47ee5e98b2159c70eea
amannarayanshukla <amanshukla18.as@gmail.com>
2021-02-25 22:48:37 +0530

    location pin hover done and text done

--- src/components/GoogleMap/LocationPin.jsx
+++ src/components/GoogleMap/LocationPin.jsx
@@ -1,13 +1,27 @@
-import { Icon } from '@iconify/react'
+import React, {useState} from 'react'
+import {Icon} from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/map-marker'
 
-const LocationPin = ({ text }) => {
-    // {console.log("----text----", text)}
+const LocationPin = ({text}) => {
+    const [isHovering, setHovering] = useState(false);
+
+    const handleMouseHover = () => {
+        setHovering(!isHovering)
+    }
+
     return (
-    <div className="pin">
-        <Icon icon={locationIcon} className="pin-icon" />
-        {/*<p className="pin-text">{text}</p>*/}
-    </div>
-)}
+        <div className="pin" onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}>
+            <Icon icon={locationIcon} className="pin-icon"/>
+            {
+                isHovering &&
+                <div>
+                    {text}
+                </div>
+            }
+            {/*<p className="pin-text">{text}</p>*/}
+        </div>
+    )
+
+}
 
 export default LocationPin
